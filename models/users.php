511b0cacdd66a83fa915636fcd3fddcbb488d4b3
kiritogtsa <tenseishitarrakenseshita@gmail.com>
2024-07-03 21:39:45 -0300

    ta ponto a primeira validaçao, criada a validaçao de cpf

--- models/users.php
+++ models/users.php
@@ -21,6 +21,9 @@ class User {
         if (empty($nome) || empty($email) || empty($senha) || empty($data_nascimento) || empty($data_adimisao) || empty($telefone) || empty($sexo) || empty($trabalho)) {
             throw new Exception("Está faltando um dado");
         }
+        if (!$this->validarcpf($cpf)) {
+            throw new Exception("nao possivel validar o cpf");
+        }
         $this->nome = $nome;
         $this->email = $email;
         $this->trabalho = $trabalho;
@@ -30,6 +33,33 @@ class User {
         $this->data_adimisao = $data_adimisao;
         $this->telefone = $telefone;
         $this->sexo = $sexo;
+    }
+    private function validarcpf($cpf){
+
+    // Extrai somente os números
+    $cpf = preg_replace( '/[^0-9]/is', '', $cpf );
+     
+    // Verifica se foi informado todos os digitos corretamente
+    if (strlen($cpf) != 11) {
+        return false;
+    }
+
+    // Verifica se foi informada uma sequência de digitos repetidos. Ex: 111.111.111-11
+    if (preg_match('/(\d)\1{10}/', $cpf)) {
+        return false;
+    }
+
+    // Faz o calculo para validar o CPF
+    for ($t = 9; $t < 11; $t++) {
+        for ($d = 0, $c = 0; $c < $t; $c++) {
+            $d += $cpf[$c] * (($t + 1) - $c);
+        }
+        $d = ((10 * $d) % 11) % 10;
+        if ($cpf[$c] != $d) {
+            return false;
+        }
+    }
+    return true;
     }
 
     // Getters
