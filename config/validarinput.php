5c573ad9cd26a3a4b2c0be70d514f9bfe901ce01
kiritogtsa <tenseishitarrakenseshita@gmail.com>
2024-07-11 07:56:04 -0300

    nada

--- config/validarinput.php
+++ config/validarinput.php
@@ -1,6 +1,4 @@
 <?php
-
-
 // valida o campos, a tecnica usada foi uma bem simples eu so crio um array com base da string recebida, e se o array for maior que um entao e invalido
 function validarinput($data){
     $datas = explode(";", $data);
