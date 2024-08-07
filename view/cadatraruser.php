aab2e9c703bf7c04d5cccd361176e7016abf7a22
kiritogtsa <tenseishitarrakenseshita@gmail.com>
2024-08-07 19:16:16 -0300

    arrumado o casdratarphp

--- view/cadatraruser.php
+++ view/cadatraruser.php
@@ -1,24 +1,34 @@
+<?php
+require_once("../models/trabalho.php");
+$cargosdao = new TrabalhoDAO();
+$cargos = $cargosdao->listarCargo();
+if (empty($cargos)) {
+    echo "sem dados";
+}
+?>
 <!DOCTYPE html>
 <html lang='en'>
+
 <head>
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>Cadastro</title>
 </head>
+
 <body>
     <form id='form' method='POST' action='../controller/main.php' class='pessoa'>
         <label for='nome'>Nome:</label>
         <input type='text' id='nome' name='nome' required>
-        
+
         <label for='cpf'>CPF:</label>
         <input type='text' id='cpf' name='cpf' required>
-        
+
         <label for='sexo'>Sexo:</label>
         <input type='text' id='sexo' name='sexo' required>
-        
+
         <label for='email'>Email:</label>
         <input type='email' id='email' name='email' required>
-        
+
         <label for='dataadmissao'>Data de Admissao:</label>
         <input type='date' id='dataadmissao' name='dataadmissao' required>
 
@@ -27,23 +37,28 @@
 
         <label for='senha'>Senha:</label>
         <input type='password' id='senha' name='senha' required>
-        
+
         <label for='telefone'>Telefone:</label>
         <input type='text' id='telefone' name='telefone' required>
 
         <label for='trabalho'>Selecione um Cargo:</label>
-        <select type ='cargoSelect' name='trabalho' required></select>
+        <select name='trabalho' required>
+            <?php
+            foreach ($cargos as $cargo) {
+            ?><option value="<?= $cargo->getIdCargo() ?>"> <?= $cargo->getNome() ?></option><?php
+                                                                                        }
+                                                                                            ?>
+        </select>
 
         <label for='grupo'>Grupo: </label>
-        <input type = 'text' name='grupo' required></input>
+        <input type='text' id='grupo' name='grupo' required>
 
-        <label for='Salário'>Salário bruto</label>
-        <input id='Salario' name='bruto' required></input>
-
+        <label for='Salario'>Salário bruto:</label>
+        <input type='text' id='Salario' name='bruto' required>
 
         <button type='submit' name='submit' value='Cadatrar_user'>Cadastrar-se</button>
     </form>
 
-    <script src='script.js'></script>
 </body>
+
 </html>
