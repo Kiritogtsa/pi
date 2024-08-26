03f886415ef88e3a5e397e3dd32eb423a0e4577d
kiritogtsa <tenseishitarrakenseshita@gmail.com>
2024-08-26 19:41:37 -0300

    mudei isso

--- view/listarusers.php
+++ view/listarusers.php
@@ -6,29 +6,30 @@ require_once('../controller/privilegios.php');
 $min = 1;
 $max = 5;
 $trabalho = new TrabalhoDAO();
-if(!empty($_SESSION['listauser'])){
+if (!empty($_SESSION['listauser'])) {
     $dados = $_SESSION['listauser']['cargos'];
-
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Buscar Funcionário</title>
     <link rel="stylesheet" type="text/css" href="arte.css" media="screen" />
 </head>
+
 <body>
     <header>
         <img src="imagens/RH.png" alt="Logo RH Connect">
         <h1>RH Connect</h1>
     </header>
     <br>
-<br>
-<br>
-<br>
+    <br>
+    <br>
+    <br>
     <!-- Formulário para buscar o funcionário -->
     <div class="container">
         <div class="formulario-exibicao-cargo">
@@ -37,38 +38,42 @@ if(!empty($_SESSION['listauser'])){
                 <input type="number" id="max" name="max" value="<?php echo $max ?>" hidden>
                 <button type="submit" value="Listar_funcionario" name="submit">Listar funcionários</button>
             </form>
-            
+
             <div class="IBuscaF">
                 <table>
                     <?php if (!empty($dados)) {
                         foreach ($dados as $d) { ?>
-                    <tr>
-                        <th>ID</th>
-                        <th>NOME</th>
-                        <th>TRABALHO</th>
-                        <th>Desativado</th>
-                    </tr>
-                                <td><?php echo $d->getId(); ?></td>
-                                <td><?php echo $d->getNome(); ?></td>
-                                <td><?php echo $trabalho->buscarPorId($d->getTrabalho()); ?></td>
-                                <td><?php echo $d->getDeletedAt(); ?></td>
-                                <td>
-                                    <form action="../controller/main.php" method="POST">
-                                        <button type="submit" value="Desativar_usuario" name="submit">Desativar funcionário</button>
-                                        <button type="submit" value="Ativar_usuariolist" name="submit">Ativar funcionário</button>
-                                    </form>
-                                </td>
+                            <tr>
+                                <th>ID</th>
+                                <th>NOME</th>
+                                <th>TRABALHO</th>
+                                <th>Desativado</th>
                             </tr>
-                        <?php }
+                            <td><?php echo $d->getId(); ?></td>
+                            <td><?php echo $d->getNome(); ?></td>
+                            <td><?php echo $trabalho->buscarPorId($d->getTrabalho()); ?></td>
+                            <td><?php echo $d->getDeletedAt(); ?></td>
+                            <td>
+                                <form action="../controller/main.php" method="POST">
+                                    <?php if ($d->getDeletedAt() == null) { ?>
+                                        <button type="submit" value="Desativar_usuario" name="submit">Desativar funcionário</button>
+                                    <?php } else { ?>
+                                        <button type="submit" value="Ativar_usuariolist" name="submit">Ativar funcionário</button>
+                                    <?php } ?>
+                                </form>
+                            </td>
+                            </tr>
+                    <?php }
                     } ?>
                 </table>
             </div>
         </div>
     </div>
-    
+
     <a href="../controller/logout.php" class="logout-icon">
         <img src="imagens/saida.png" alt="Logout">
     </a>
     <a href="welcomeadmins.php" class="back-button">Voltar</a>
 </body>
+
 </html>
